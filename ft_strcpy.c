1a083d82b724622fec00ddf58c5cef25b9025684
Siyabonga Mzimela <smzimela@c4r12s1.wethinkcode.co.za>
2020-06-26 11:53:44 +0200

    done

---
+++ ft_strcpy.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+//#include <stdio.h>
+
+char *ft_strcpy(char *dest , char *src)
+{
+	*dest = *src ;
+	return  dest;
+
+}
+
+/*
+int main()
+{
+	char *s1[] = "Hello";
+    char *s2[]="";
+   
+	ft_strcpy(s2,s1);
+	printf("%s",s2);
+	return 0;
+}
+*/
+
